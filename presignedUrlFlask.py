b5033441e0d7749fc478971a6bb8a84ed4961944
rphil04 <64931748+rphil04@users.noreply.github.com>
2023-08-31 19:25:14 -0500

    Update presignedUrlFlask.py
    Summary:
    
    The provided code defines a Flask web application that generates and displays a presigned URL for an image stored in an Amazon S3 bucket. The Flask app includes two main components:
    
    Presigned URL Generation:
    
    A function presigned_s3_url generates a presigned URL for accessing an S3 object securely.
    It takes a bucket name, object key, and expiration time as parameters.
    The function uses the boto3 library to create a presigned URL with limited-time access to the S3 object.
    Web Application Display:
    
    The app defines a route for the root URL '/'.
    The index function generates a presigned URL using presigned_s3_url.
    It creates an HTML template that displays an image fetched from the presigned URL.
    When the root URL is accessed, the app responds with the generated HTML page.
    The Flask app is set to run when the script is executed directly. The provided code snippet improves the understanding of the script's functionality and structure.

--- presignedUrlFlask.py
+++ presignedUrlFlask.py
@@ -1,41 +1,43 @@
+# Import necessary modules
 import os
 import boto3
-
 from flask import Flask, session, redirect, escape, request
 
-# Create the pre-signed URL
-def presigned_s3_url(bucket, key, expiration):
-    """ Ggenerate presigned URL on S3 (Default 1 hour expiration)
-    """
-    params = {
-        'Bucket': bucket,
-        'Key': key
-    }
-    s3 = boto3.client('s3')
-    url = s3.generate_presigned_url('get_object', Params=params, ExpiresIn=expiration)
-    return(url)
-
-# Configure the application name with the FLASK_APP environment variable.
+# Create Flask application instance
 app = Flask(__name__)
 
-
-# Configure the secret_key with the SECRET_KEY environment variable.
+# Configure the secret key with the SECRET_KEY environment variable
 app.secret_key = os.environ.get('SECRET_KEY', default=None)
 
+# Generate presigned URL for an S3 object
+def presigned_s3_url(bucket, key, expiration):
+    params = {'Bucket': bucket, 'Key': key}
+    s3 = boto3.client('s3')
+    url = s3.generate_presigned_url('get_object', Params=params, ExpiresIn=expiration)
+    return url
+
+# Define a route for the root URL
 @app.route('/')
 def index():
+    # Generate presigned URL for an image in S3 bucket
     pre_signed_url = presigned_s3_url('pre-signed-url-lab', 'ScatterChart.png', 3660)
-    html_out = '''<!DOCTYPE html> 
-                    <html> 
-                      <head> 
-                        <title>
-                           HTML img src Attribute 
-                        </title>
-                      </head>
-                      <body>
-                        <h1>Image from pre-signed URL</h1>
-                        <img src= {0} alt="Pre-Signed URL generated image link"> 
-                      </body>
-                      </html>'''.format(pre_signed_url)
+    
+    # Create an HTML template to display the image
+    html_out = '''
+        <!DOCTYPE html> 
+        <html> 
+          <head> 
+            <title>HTML img src Attribute</title>
+          </head>
+          <body>
+            <h1>Image from presigned URL</h1>
+            <img src="{0}" alt="Pre-Signed URL generated image link"> 
+          </body>
+        </html>
+    '''.format(pre_signed_url)
 
     return html_out
+
+# Run the Flask application if script is executed directly
+if __name__ == '__main__':
+    app.run()
